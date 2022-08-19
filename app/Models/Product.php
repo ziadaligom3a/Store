87c587b482ffc433a8c195892a37e1f1a19bf019
ziadaligom3a <ajzead660@gmail.com>
2022-08-19 18:55:06 +0200

    Updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,7 +18,7 @@ class Product extends Model
 
     public function make(){
 
-        Customers::create([
+        return Customers::create([
             'point' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it lookIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look
             ",
             'name' => 'JAMES DEW',
