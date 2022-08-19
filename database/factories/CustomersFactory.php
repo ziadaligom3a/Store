3a7996512f41229651014942a3b5c3819a305524
ziadaligom3a <ajzead660@gmail.com>
2022-08-19 15:03:28 +0200

    Latest Update

--- database/factories/CustomersFactory.php
+++ database/factories/CustomersFactory.php
@@ -17,7 +17,12 @@ class CustomersFactory extends Factory
     public function definition()
     {
         return [
-            //
+            
+            'point' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it lookIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look
+            ",
+            'name' => 'JAMES DEW',
+            'img' => 'https://iili.io/gM41zF.jpg',
+            'pro' => 'Photographer'
         ];
     }
 }
--- database/migrations/2022_08_16_191755_create_customers_table.php
+++ database/migrations/2022_08_16_191755_create_customers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('point');
+            $table->text('point');
             $table->string('name');
             $table->string('img');
             $table->string('pro');
